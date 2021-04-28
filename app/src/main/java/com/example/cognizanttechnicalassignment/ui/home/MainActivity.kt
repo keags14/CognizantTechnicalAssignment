d0d2158ae9d02bb44bd8f6b4a445985d0901ede5
keagan.gonsalves@appexperts.me.uk <Hatfield2021!>
2021-04-28 09:01:22 +0100

    list view added with adapter, data source for the listview.

--- app/src/main/java/com/example/cognizanttechnicalassignment/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/cognizanttechnicalassignment/ui/home/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.cognizanttechnicalassignment.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cognizanttechnicalassignment.R
 
+
 class MainActivity : AppCompatActivity() {
+
+    // Array of strings...
+    var avengersArray = arrayOf(
+        "Captain America", "Iron Man", "Hulk", "Black Widow",
+        "Thor", "Spider Man", "Thanos", "Dr.Strange"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter<String>(this, R.layout.avengers_listview, avengersArray)
+        val listView: ListView = findViewById<View>(R.id.list_view) as ListView
+        listView.adapter = adapter
     }
 }
