807258214d1fc151a33106ce2e79073ea67d8b25
keagan.gonsalves@appexperts.me.uk <Hatfield2021!>
2021-04-28 08:19:16 +0100

    Added the main home page with the listview.

--- app/src/main/java/com/example/cognizanttechnicalassignment/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/cognizanttechnicalassignment/ui/login/LoginActivity.kt
@@ -1,11 +1,8 @@
 package com.example.cognizanttechnicalassignment.ui.login
 
 import android.app.Activity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.StringRes
-import androidx.appcompat.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -14,8 +11,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.cognizanttechnicalassignment.R
+import com.example.cognizanttechnicalassignment.ui.home.MainActivity
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -62,20 +64,24 @@ class LoginActivity : AppCompatActivity() {
 
             //Complete and destroy login activity once successful
             finish()
+
+            // Start Main Activity
+            val redirect = Intent(this, MainActivity::class.java)
+            startActivityForResult(redirect, Activity.RESULT_OK)
         })
 
         username.afterTextChanged {
             loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
+                username.text.toString(),
+                password.text.toString()
             )
         }
 
         password.apply {
             afterTextChanged {
                 loginViewModel.loginDataChanged(
-                        username.text.toString(),
-                        password.text.toString()
+                    username.text.toString(),
+                    password.text.toString()
                 )
             }
 
@@ -83,8 +89,8 @@ class LoginActivity : AppCompatActivity() {
                 when (actionId) {
                     EditorInfo.IME_ACTION_DONE ->
                         loginViewModel.login(
-                                username.text.toString(),
-                                password.text.toString()
+                            username.text.toString(),
+                            password.text.toString()
                         )
                 }
                 false
@@ -102,9 +108,9 @@ class LoginActivity : AppCompatActivity() {
         val displayName = model.displayName
         // TODO : initiate successful logged in experience
         Toast.makeText(
-                applicationContext,
-                "$welcome $displayName",
-                Toast.LENGTH_LONG
+            applicationContext,
+            "$welcome $displayName",
+            Toast.LENGTH_LONG
         ).show()
     }
 
